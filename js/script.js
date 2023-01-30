68cdb0380bfbf982ee999cda6a4f901a5dd247dd
gvtbble <junkeeeeer@gmail.com>
2023-01-30 18:11:02 +0300

    null

---
+++ js/script.js
@@ -0,0 +1,9 @@
+
+
+    //IBG
+	document.querySelectorAll(".ibg").forEach(el => {
+		if(el.querySelector('img')){
+			el.style.backgroundImage = 'url('+el.querySelector('img').getAttribute('src')+')';
+			el.querySelector('img').style.display = 'none';
+		}
+	});
