79890313fb0c1776e72b362844da6113f61dbaa9
gvtbble <junkeeeeer@gmail.com>
2023-02-07 17:54:24 +0300

    working

--- js/script.js
+++ js/script.js
@@ -1,9 +1,20 @@
+// let myArr=[];
+// let menuIcon = document.querySelector(".menu__icon");
+// let body = document.querySelector("body");
+// myArr.push(menuIcon)
+// myIcon.addEventListener("click",()=>{
+// 	myArr.forEach(element=>{
+// 		element.classList.toggle("active")
+// 	});
+// 	body.classList.toggle("lock")
+// })
 
 
-    //IBG
-	document.querySelectorAll(".ibg").forEach(el => {
-		if(el.querySelector('img')){
-			el.style.backgroundImage = 'url('+el.querySelector('img').getAttribute('src')+')';
-			el.querySelector('img').style.display = 'none';
-		}
+let iconMenu=document.querySelector(".icon-menu");
+		let body=document.querySelector("body");
+		let menuBody=document.querySelector(".menu__body");
+	iconMenu.addEventListener("click", (e) => {
+		iconMenu.classList.toggle("active");
+		body.classList.toggle("lock");
+		menuBody.classList.toggle("active");
 	});
