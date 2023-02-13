89cd21f9e2602dc55cfd7a2e296bb9fcc53b3a2d
gvtbble <junkeeeeer@gmail.com>
2023-02-13 17:14:31 +0300

    fixed + add slider

--- js/script.js
+++ js/script.js
@@ -1,23 +1,14 @@
-// let myArr=[];
-// let menuIcon = document.querySelector(".menu__icon");
-// let body = document.querySelector("body");
-// myArr.push(menuIcon)
-// myIcon.addEventListener("click",()=>{
-// 	myArr.forEach(element=>{
-// 		element.classList.toggle("active")
-// 	});
-// 	body.classList.toggle("lock")
-// })
-function ibg(){
-	$.each($('.ibg'), function(index, val) {
-		if($(this).find('img').length>0){
-			$(this).css('background-image','url("'+$(this).find('img').attr('src')+'")');
-		}
-	});
-}
-ibg();
 
-let iconMenu=document.querySelector(".icon-menu");
+let ready = (callback) => {
+	if (document.readyState != "loading") callback();
+	else document.addEventListener("DOMContentLoaded", callback);
+}
+
+
+ready(() => {
+
+	//MENU
+		let iconMenu=document.querySelector(".icon-menu");
 		let body=document.querySelector("body");
 		let menuBody=document.querySelector(".menu__body");
 	iconMenu.addEventListener("click", (e) => {
@@ -26,5 +17,57 @@ let iconMenu=document.querySelector(".icon-menu");
 		menuBody.classList.toggle("active");
 	});
 
+	//SLIDER
+	if($('.slider__body').length>0){
+		$('.slider__body').slick({
+			autoplay: true,
+			infinite: false,
+			dots: true,
+			arrows: false,
+			accessibility:false,
+			slidesToShow:1,
+			autoplaySpeed: 3000,
+			adaptiveHeight:true,
+			nextArrow:'<button type="button" class="slick-next"></button>',
+			prevArrow:'<button type="button" class="slick-prev"></button>',
+			responsive: [{
+				breakpoint: 768,
+				settings: {}
+			}]
+		});
+	}
 
-	
+		let slider=document.querySelectorAll(".slider");
+		slider.forEach(el => {
+			let sliderItem = new Glide(el, {
+				type: 'carousel',
+				perView: 1,
+			});
+		sliderItem.mount();
+	});
+
+	//IBG
+	document.querySelectorAll(".ibg").forEach(el => {
+		if(el.querySelector('img')){
+			el.style.backgroundImage = 'url('+el.querySelector('img').getAttribute('src')+')';
+			el.querySelector('img').style.display = 'none';
+		}
+	});
+});
+
+
+/*
+
+		let element = document.createElement("div");
+	element.classList.add("content", "active");
+	element.innerHTML = "<strong>Всем привет!</strong> Вы прочитали важное сообщение.";
+	document.querySelector(".mainblock").append(element);
+
+	if(iconMenu.classList.contains('active')){
+		
+	}
+	document.querySelectorAll(".box")
+		.forEach(el => {
+			el.style.display = "none"
+		});
+*/
